e1a0420ceb5d68226a159ef3b0572f5405c10ece
Joy William <jw42205769@gmail.com>
2026-06-08 15:53:02 +0300

    Update and rename app.jsx to app.js

--- app.js
+++ app.js
@@ -4,17 +4,25 @@ import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import Services from "./pages/Services";
 
 
 function App() {
 
   return (
+
     <BrowserRouter>
 
       <Routes>
 
-        <Route path="/" element={<Home />} />
+        {/* Home */}
+        <Route 
+          path="/" 
+          element={<Home />} 
+        />
 
+
+        {/* Authentication */}
         <Route 
           path="/register" 
           element={<Register />} 
@@ -25,15 +33,27 @@ function App() {
           element={<Login />} 
         />
 
+
+        {/* User Area */}
         <Route 
           path="/dashboard" 
           element={<Dashboard />} 
         />
 
+
+        {/* Services */}
+        <Route 
+          path="/services" 
+          element={<Services />} 
+        />
+
+
       </Routes>
 
     </BrowserRouter>
+
   );
+
 }
 
 
